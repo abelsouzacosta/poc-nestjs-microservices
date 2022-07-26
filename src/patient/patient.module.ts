b05de7e3f7d4975449a6887bd01787e916a451ad
abelsouzacosta <abelsouzacosta@gmail.com>
2022-07-26 15:42:46 -0300

    chore: add MulterModule dependency

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -5,12 +5,17 @@ import { PatientRepository } from './patient.repository';
 import { PatientService } from './patient.service';
 import { PatientController } from './patient.controller';
 import { AddressService } from './address.service';
+import { ImportService } from './import.service';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Patient.name, schema: PatientSchema }]),
+    MulterModule.register({
+      dest: './uploads',
+    }),
   ],
-  providers: [PatientRepository, PatientService, AddressService],
+  providers: [PatientRepository, PatientService, AddressService, ImportService],
   controllers: [PatientController],
 })
 export class PatientModule {}
