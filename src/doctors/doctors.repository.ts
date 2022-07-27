5121a8542139a22c5720ab05b8a0889efada7789
abelsouzacosta <abelsouzacosta@gmail.com>
2022-07-27 06:27:53 -0300

    chore: add repository and method to list all doctors

---
+++ src/doctors/doctors.repository.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Doctor } from './entities/doctor.entity';
+
+@Injectable()
+export class DoctorRepository {
+  constructor(
+    @InjectModel(Doctor.name) private readonly model: Model<Doctor>,
+  ) {}
+
+  async getAll(): Promise<Array<Doctor>> {
+    return this.model.find();
+  }
+}
