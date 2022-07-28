1e75b47b5289269c652d52cfc63774b5cb9ffe1f
abelsouzacosta <abelsouzacosta@gmail.com>
2022-07-28 20:37:15 -0300

    chore: add service for medicine

---
+++ src/medicines/medicines.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMedicineDto } from './dto/create-medicine.dto';
+import { UpdateMedicineDto } from './dto/update-medicine.dto';
+import { MedicinesRepository } from './medicines.repository';
+
+@Injectable()
+export class MedicinesService {
+  constructor(private readonly repository: MedicinesRepository) {}
+
+  create(createMedicineDto: CreateMedicineDto) {
+    return this.repository.create(createMedicineDto);
+  }
+
+  findAll() {
+    return `This action returns all medicines`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} medicine`;
+  }
+
+  update(id: number, updateMedicineDto: UpdateMedicineDto) {
+    return `This action updates a #${id} medicine`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} medicine`;
+  }
+}
