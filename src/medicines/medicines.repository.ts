efa095de62dbae16d5a7766137af2c9f973a48e3
abelsouzacosta <abelsouzacosta@gmail.com>
2022-07-28 23:54:09 -0300

    chore: add method to repository

--- src/medicines/medicines.repository.ts
+++ src/medicines/medicines.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateMedicineDto } from './dto/create-medicine.dto';
+import { UpdateMedicineDto } from './dto/update-medicine.dto';
 import { Medicine } from './entities/medicine.entity';
 
 @Injectable()
@@ -28,5 +29,23 @@ export class MedicinesRepository {
 
   async create(medicine: CreateMedicineDto): Promise<Medicine> {
     return this.model.create(medicine);
+  }
+
+  async update(
+    id: string,
+    { name, dosage, active_ingredient, brand, manufacturer }: UpdateMedicineDto,
+  ) {
+    return this.model.updateOne(
+      {
+        _id: id,
+      },
+      {
+        name,
+        dosage,
+        active_ingredient,
+        brand,
+        manufacturer,
+      },
+    );
   }
 }
