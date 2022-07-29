3d9532e61189796ee1a9c2da3d4661cb8b0a78e0
abelsouzacosta <abelsouzacosta@gmail.com>
2022-07-28 23:55:04 -0300

    refactor: remove fields from dto

--- src/medicines/dto/update-medicine.dto.ts
+++ src/medicines/dto/update-medicine.dto.ts
@@ -1,4 +1,33 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMedicineDto } from './create-medicine.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateMedicineDto extends PartialType(CreateMedicineDto) {}
+export class UpdateMedicineDto {
+  @IsString({
+    message: 'name should be a string',
+  })
+  @IsOptional()
+  name?: string;
+
+  @IsString({
+    message: 'dosage should be a string',
+  })
+  @IsOptional()
+  dosage?: string;
+
+  @IsString({
+    message: 'active_ingredient should be a string',
+  })
+  @IsOptional()
+  active_ingredient?: string;
+
+  @IsString({
+    message: 'brand should be a string',
+  })
+  @IsOptional()
+  brand?: string;
+
+  @IsString({
+    message: 'manufacturer should be a string',
+  })
+  @IsOptional()
+  manufacturer?: string;
+}
