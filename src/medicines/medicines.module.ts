4e12d7d6fc90cd23156c9681a56c518946d59a4b
abelsouzacosta <abelsouzacosta@gmail.com>
2022-08-01 22:32:13 -0300

    chore: add multer to module

--- src/medicines/medicines.module.ts
+++ src/medicines/medicines.module.ts
@@ -4,14 +4,19 @@ import { MedicinesController } from './medicines.controller';
 import { MedicinesRepository } from './medicines.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Medicine, MedicineSchema } from './entities/medicine.entity';
+import { MulterModule } from '@nestjs/platform-express';
+import { ImportService } from 'src/medicines/import.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Medicine.name, schema: MedicineSchema },
     ]),
+    MulterModule.register({
+      dest: './uploads/medicines',
+    }),
   ],
   controllers: [MedicinesController],
-  providers: [MedicinesService, MedicinesRepository],
+  providers: [MedicinesService, MedicinesRepository, ImportService],
 })
 export class MedicinesModule {}
