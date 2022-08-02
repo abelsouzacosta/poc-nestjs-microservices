502d0ecb0f7830b14f04707751883d009970e0de
abelsouzacosta <abelsouzacosta@gmail.com>
2022-08-02 07:48:32 -0300

    refactor: remove files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PatientModule } from './patient/patient.module';
 import { DoctorsModule } from './doctors/doctors.module';
 import { MedicinesModule } from './medicines/medicines.module';
@@ -14,7 +12,7 @@ import 'dotenv/config';
     DoctorsModule,
     MedicinesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
