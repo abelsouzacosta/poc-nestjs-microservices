e368efe0d18aab9e7a56f7d7198925478f77d983
Abel Junior <abel.costa@webjump.com.br>
2022-08-04 11:26:14 -0300

    feat(medicines): middleware
    adding middleware to objects that are not found

---
+++ src/medicines/middlewares/medicine-not-found.middleware.ts
@@ -0,0 +1,27 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { MedicinesRepository } from '../medicines.repository';
+
+@Injectable()
+export class MedicineNotFoundMiddleware implements NestMiddleware {
+  constructor(private readonly repository: MedicinesRepository) {}
+
+  async use(request: Request, response: Response, next: NextFunction) {
+    const { id } = request.params;
+
+    const medicineFound = await this.repository.findById(id);
+
+    if (!medicineFound)
+      throw new HttpException(
+        `Mecicine not found with id ${id}`,
+        HttpStatus.NOT_FOUND,
+      );
+
+    next();
+  }
+}
