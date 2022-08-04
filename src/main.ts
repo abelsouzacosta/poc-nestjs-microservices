26f1057c7bf73e4ca657a8ed5995d4e416658dac
Abel Junior <abel.costa@webjump.com.br>
2022-08-04 15:34:45 -0300

    chore: binding custom exception filter for the application

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './shared/infra/filters/exceptions/http.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     abortOnError: false,
   });
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
